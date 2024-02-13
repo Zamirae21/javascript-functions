60e0b10dda02ccca9b66c38f964d7a873d8fd169
Zamira Estrada <156048180+Zamirae21@users.noreply.github.com>
2024-02-13 12:09:45 -0500

    push and control
    moving to main branch

--- JavaScript script.js
+++ JavaScript script.js
@@ -1,1 +1,25 @@
+// Define a function named firstLetterName
+function firstLetterName(name) {
+    var firstLetter = name.charAt(0);
+    alert("The name " + name + " starts with the letter " + firstLetter);
+}
 
+// Define a function named divisibleByTwo
+function divisibleByTwo(number) {
+    if (number % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Define a function named largestNum
+function largestNum(arr) {
+    var largest = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
